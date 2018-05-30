736cc589324941dbda32bf0161c3e727da871e6b
bunkermaster <yann.le-scouarnec@hetic.net>
2018-05-30 11:21:18 +0200

    Base structure files

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author Yann Le Scouarnec <bunkermaster@gmail.com>
+ * Date: 30/05/2018
+ * Time: 11:19
+ */
+require_once dirname(__DIR__)."/index.php";
+
+echo KANDT_ROOT_DIR;
